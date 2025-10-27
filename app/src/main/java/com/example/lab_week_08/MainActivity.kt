6c197ddd255947bf98cc5ed0931eb895e8e0422b
aisyai <aisyaadiyann@gmail.com>
2025-10-27 15:57:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lab_week_08
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    //Create an instance of a work manager
+    //Work manager manages all your requests and workers
+    //it also sets up the sequence for all your processes
+    private val workManager = WorkManager.getInstance(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
